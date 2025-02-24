0e72918a87ef9ada3cc2b7eaf627963bfe2fd238
JBernal <j.bernal@upm.es>
2025-02-24 06:49:54 +0100

    added login

--- lightsail-install-docker.ps1
+++ lightsail-install-docker.ps1
@@ -2,8 +2,10 @@ ssh -i "LightsailDefaultKey-REGION.pem" ubuntu@ip.ip.ip.ip
 
 lsb_release -a
 echo $USER
+
 sudo apt update
 sudo apt install -y docker.io
 sudo usermod -aG docker $USER
 newgrp docker
+
 docker --version
